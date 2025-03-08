17d903971b43a584514445cf2a3e62c05527cb30
junjie-w <junjiewu.brooke@gmail.com>
2025-03-08 08:47:55 +0100

    feat: project setup

---
+++ src/main/java/com/example/jwt_auth_server/JwtAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt_auth_server/JwtAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
